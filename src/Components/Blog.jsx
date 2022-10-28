454b9d3685ceb8e2c96f816d71b55bff348de060
Md. Mofazzel Hossen <86169875+moofazzel@users.noreply.github.com>
2022-10-28 23:01:47 +0600

    faq and blog page added

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -2,61 +2,64 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <div>
+    <>
       <section className="">
+      <div className="text-center text-5xl font-bold py-10">Blog</div>
+
         <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
-          
           <div className="space-y-4">
             <details className="w-full border rounded-lg" open="">
               <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
-                What is the purpose react router?
+                What is cors?
               </summary>
               <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-                React Router is a standard library for routing in React. It
-                enables the navigation among views of various components in a
-                React Application, allows changing the browser URL, and keeps
-                the UI in sync with the URL.
+                Cross-origin resource sharing (CROS) is a mechanism that allows
+                restricted resources on a web page to be requested from another
+                domain outside the domain from which the first resource was
+                served. A web page may freely embed cross-origin images,
+                stylesheets, scripts, iframes, and videos.
               </p>
             </details>
             <details className="w-full border rounded-lg" open="">
               <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
-                React Context API How it works?
+                Why are you using firebase? What other options do you have to
+                implement authentication?
               </summary>
               <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-                React.createContext() is all you need. It returns a consumer and
-                a provider. Provider is a component that as it's names suggests
-                provides the state to its children. It will hold the "store" and
-                be the parent of all the components that might need that store.
-                Consumer as it so happens is a component that consumes and uses
-                the state.
+                Firebase Authentication provides backend services, easy-to-use
+                SDKs, and ready-made UI libraries to authenticate users to your
+                app. It supports authentication using passwords, phone numbers,
+                popular federated identity providers like Google, Facebook and
+                Twitter, and more. The best Firebase alternatives are: Auth0,
+                MongoDB. MongoDB,Passport, Okta, JSON Web Token, Keycloak,
+                Amazon Cognito.
               </p>
             </details>
             <details className="w-full border rounded-lg" open="">
               <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
-                What is use REF IN React?
+                How does the private route work?
               </summary>
               <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
-                Refs provide a way to access DOM nodes or React elements created
-                in the -RENDER method. In the typical React dataflow, props are
-                the only way that parent components interact with their
-                children. To modify a child, you re-render it with new props.
-                However, there are a few cases where you need to imperatively
-                modify a child outside of the typical dataflow. The child to be
-                modified could be an instance of a React component, or it could
-                be a DOM element. For both of these cases, React provides an
-                escape hatch. When to Use Refs There are a few good use cases
-                for refs: Managing focus, text selection, or media playback.
-                Triggering imperative animations. Integrating with third-party
-                DOM libraries. Avoid using refs for anything that can be done
-                declaratively. For example, instead of exposing open() and
-                close() methods on a Dialog component, pass an isOpen prop to
-                it.
+                How does the private route work? The private route component is
+                similar to the public route, the only change is that redirect
+                URL and authenticate condition. If the user is not authenticated
+                he will be redirected to the login page and the user can only
+                access the authenticated routes If he is authenticated (Logged
+                in).
+              </p>
+            </details>
+            <details className="w-full border rounded-lg" open="">
+              <summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">
+              What is Node? How does Node work?
+              </summary>
+              <p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">
+              It is a used as backend service where javascript works on the server-side of the application. This way javascript is used on both frontend and backend. Node. js runs on chrome v8 engine which converts javascript code into machine code, it is highly scalable, lightweight, fast, and data-intensive.
               </p>
             </details>
           </div>
         </div>
       </section>
-    </div>
+    </>
   );
 };
 
